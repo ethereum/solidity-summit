6690fa66e44c9b40dd77439cdd2470d45731258b
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-04-22 08:25:14 +0200

    Adding leading zero for counter values

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,5 +1,10 @@
+function pad2(num) {
+    var s = "0" + num;
+    return s.substr(s.length - 2);
+}
+
 var deadline = new Date("2020-04-29T13:00:00.000+01:00");
-  
+
 var x = setInterval(function() { 
     var now = new Date().getTime(); 
     var t = deadline - now; 
@@ -9,16 +14,16 @@ var x = setInterval(function() {
     var seconds = Math.floor((t % (1000 * 60)) / 1000); 
     
     [].forEach.call(document.getElementsByClassName("days"), function (el) {
-        el.innerHTML = days;
+        el.innerHTML = pad2(days);
     });
     [].forEach.call(document.getElementsByClassName("hours"), function (el) {
-        el.innerHTML = hours;
+        el.innerHTML = pad2(hours);
     });
     [].forEach.call(document.getElementsByClassName("minutes"), function (el) {
-        el.innerHTML = minutes;
+        el.innerHTML = pad2(minutes);
     });
     [].forEach.call(document.getElementsByClassName("seconds"), function (el) {
-        el.innerHTML = seconds;
+        el.innerHTML = pad2(seconds);
     });
     if (t < 0) { 
         clearInterval(x);
